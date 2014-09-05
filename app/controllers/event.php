78eda5d24f1b9602039236368ab08bc406081d07
chazmuzz <charliemurray37@gmail.com>
2014-09-05 14:30:19 +0100

    PHP 5.6 'use function'

--- app/controllers/event.php
+++ app/controllers/event.php
@@ -1,46 +1,49 @@
 <?php
 
-deep\routes\handle_page('event', function($request, $response) {
+use function deep\routes\handle_page;
+use function deep\routes\handle_action;
+
+handle_page('event', function($request, $response) {
 	$user = $request->user;
 
-	deep\routes\handle_action('default', function() use ($request, $response, $user) {
+	handle_action('default', function() use ($request, $response, $user) {
 		$response->foo = "foo";
 		$response->user = $request->user;
 	});
 
-	deep\routes\handle_action('create', function() use ($request, $response, $user) {
-		$event = \bm\model\event\make();
+	handle_action('create', function() use ($request, $response, $user) {
+		$event = bm\model\event\make();
 		if ($request->event) {
 			foreach ($request->event as $key => $value) {
 				$event->$key = $value;
 			}
 		}
-		$response->event = \bm\model\event\save($event);
+		$response->event = bm\model\event\save($event);
 	});
 
-	deep\routes\handle_action('read', function() use ($request, $response, $user) {
+	handle_action('read', function() use ($request, $response, $user) {
 		if ($request->id) {
-			$response->events = \bm\model\event\get($request->id);
+			$response->events = bm\model\event\get($request->id);
 		} else {
 			$user = $request->user;
-			$response->events = \bm\model\event\get($user, $request->start, $request->end);
+			$response->events = bm\model\event\get($user, $request->start, $request->end);
 		}
 	});
  
-	deep\routes\handle_action('update', function() use ($request, $response, $user) {
-		$event = \bm\model\event\get($request->event->id);
+	handle_action('update', function() use ($request, $response, $user) {
+		$event = bm\model\event\get($request->event->id);
 		if ($request->event) {
 			foreach ($request->event as $key => $value) {
 				$event->$key = $value;
 			}
 		}
-		$response->event = \bm\model\event\save($event);
+		$response->event = bm\model\event\save($event);
 	});
 
-	deep\routes\handle_action('destroy', function() use ($request, $response, $user) {
-		$event = \bm\model\event\get($request->event->id);
+	handle_action('destroy', function() use ($request, $response, $user) {
+		$event = bm\model\event\get($request->event->id);
 		$response->event = $request->event->id;
-		$response->destroyed = \bm\model\event\destroy($event);
+		$response->destroyed = bm\model\event\destroy($event);
 	});
 });
 
